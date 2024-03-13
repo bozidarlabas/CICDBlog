eb1aaa0e04673729e665d4ee66a243f1471b86d0
Bozidar <bozidar@inquis-it.hr>
2024-03-13 15:04:14 +0100

    sdf

--- CICDBlogTests/CICDBlogTests.swift
+++ CICDBlogTests/CICDBlogTests.swift
@@ -19,7 +19,7 @@ final class CICDBlogTests: XCTestCase {
     }
 
     func testExample() throws {
-        XCTAssertEqual("Test12", "Hello, Worldd222!")
+        XCTAssertEqual("Test12g", "Hello, Worldd222!")
     }
 
     func testExample2() throws {
